697b9419b2b015bf172e2a128444cfb83990fbe4
kuka <olzhabaikuanysh257@gmail.com>
2026-02-17 13:12:46 +0500

    Add files via upload

---
+++ lab4/car.service.ts
@@ -0,0 +1,18 @@
+/* Creating an injective service
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CarService {
+  cars = ['Sunflower GT', 'Flexus Sport', 'Sprout Mach One'];
+
+  getCars(): string[] {
+    return this.cars;
+  }
+
+  getCar(id: number) {
+    return this.cars[id];
+  }
+}
+*/
